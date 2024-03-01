9bf9bcc6a2d51224697518d3a96147bc48cbb3e7
KumihoX <v.dombrovskaya_1998@mail.ru>
2024-03-02 01:15:11 +0700

    made the basis of the navigation graph
    3 screens have been created:
    - SignIn
    - Main
    - History

---
+++ svc_Employee_client/app/src/main/java/com/example/employeeclient/ui/auth/AuthActivity.kt
@@ -0,0 +1,17 @@
+package com.example.employeeclient.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.employeeclient.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+}
