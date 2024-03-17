56d71c1e25f699b476d79bb078f1e8cf25fe02fa
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2024-03-17 16:17:55 +0700

    Add cache libs

---
+++ Libraries/Bank.Common/Cache/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace Bank.Common.Cache;
+
+public interface ICache<TKey, TValue>
+{
+    bool TryGet(TKey key, DateTime expiresAt, out TValue? value);
+    void AddOrUpdate(TKey key, TValue value, DateTime expiresAt);
+    void Clear();
+}
