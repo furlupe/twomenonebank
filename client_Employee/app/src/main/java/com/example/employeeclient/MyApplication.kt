30d2a8e4ed6a1148177f555d181740bf3ad167e2
Sofiya Velikanova <solntselikaya.sofia@yandex.ru>
2024-04-20 22:54:45 +0700

    Added notifications

--- client_Employee/app/src/main/java/com/example/employeeclient/MyApplication.kt
+++ client_Employee/app/src/main/java/com/example/employeeclient/MyApplication.kt
@@ -1,6 +1,11 @@
 package com.example.employeeclient
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import com.example.employeeclient.common.Constants.LOCAL_CHANNEL
+import com.example.employeeclient.common.Constants.LOCAL_CHANNEL_ID
 import com.example.employeeclient.di.appModule
 import com.example.employeeclient.di.useCaseModule
 import com.example.employeeclient.di.viewModelModule
@@ -8,12 +13,27 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
 class MyApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
+
+        createNotificationChannel()
 
         startKoin {
             androidContext(this@MyApplication)
             modules(appModule, useCaseModule, viewModelModule, )
         }
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            LOCAL_CHANNEL_ID,
+            LOCAL_CHANNEL,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        // Register the channel with the system.
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
